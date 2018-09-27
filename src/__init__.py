6e84e5c02b37cba555e70e0be4b9e5abf5853648
zthxxx <zthxxx.me@gmail.com>
2018-09-27 23:24:55 +0800

    fix: fix build with readthedocs

--- src/__init__.py
+++ src/__init__.py
@@ -10,12 +10,21 @@ __all__ = find_packages(path.dirname(__file__))
 # but only import_hook designed to compatible with python2.7
 
 
+def try_locale_chain(locales):
+    for item in locales:
+        try:
+            return locale.setlocale(locale.LC_ALL, item)
+        except locale.Error:
+            pass
+
+
 # recommend to set 'LC_ALL=en_US.UTF-8'
 # it's must if use click
 # also set 'LC_ALL=en_US.UTF-8' in pycharm environment or system env
 # PyCharm -> Run -> Edit Configurations -> Defaults -> Python -> Environment variables
 # $ export LC_ALL=en_US.UTF-8
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+try_locale_chain(('en_US.UTF-8', 'en_US', 'C'))
 
 
 def get_version():
